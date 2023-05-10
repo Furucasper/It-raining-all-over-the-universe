23ebece5c86573f34a45d32288020ca64ddfc2a0
Furucasper <furuku.102@gmail.com>
2023-05-10 16:12:38 +0700

    fix: back button on cookie policy page

--- src/pages/cookiepolicy.tsx
+++ src/pages/cookiepolicy.tsx
@@ -4,8 +4,8 @@ import { useNavigate } from 'react-router-dom';
 const CookiePolicy: FC = () => {
     const navigate = useNavigate()
     return (
-        <div className='bg-white p-6 font-season h-full w-full overflow-y-auto'>
-            <div className='overlay top-8 left-4 bg-white p-1 pl-0 rounded-md'>
+        <div className='bg-white p-6 pt-8 font-season h-full w-full overflow-y-auto'>
+            <div className='overlay top-10 left-4 bg-white p-1 pl-0 rounded-md'>
                 <button className='drop-shadow text-lg font-bold bg-zinc-200 hover:bg-zinc-300 rounded-md px-2 py-1 cursor-pointer m-x-auto' onClick={() => navigate(-1)}>&lt;- ย้อนกลับ</button>
             </div>
             <header className='text-center pt-4'>
