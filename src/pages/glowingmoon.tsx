d7955437c4a6eae6342cafb99b415d36aa49af55
Furucasper <furuku.102@gmail.com>
2023-05-11 23:59:53 +0700

    👀Change bg from mp4 to gif in glowing moon page + Add prefetching

--- src/pages/glowingmoon.tsx
+++ src/pages/glowingmoon.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import BlankPage from './blankpage';
 import { useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
@@ -6,65 +6,55 @@ import { Helmet } from 'react-helmet';
 const GlowingMoon: FC = () => {
 
     const navigate = useNavigate()
-    const videoPlayer = useRef<HTMLVideoElement>(null);
-    const videoPlayer2 = useRef<HTMLVideoElement>(null);
+    const [src, setSrc] = useState<string>('/pages/glowing-moon.gif')
+    const [show, setShow] = useState<boolean>(false)
 
     useEffect(() => {
-        //adjest video speed to 0.75
-        if (videoPlayer.current) {
-            videoPlayer.current.playbackRate = 0.75;
+        setTimeout(() => {
+            setShow(true)
             setTimeout(() => {
-                videoPlayer.current?.play()
-            }, 3000)
-        }
-        if (videoPlayer2.current) {
-            setTimeout(() => {
-                videoPlayer2.current?.play()
-            }, 10000)
-            videoPlayer2.current.addEventListener('ended', () => {
+                setSrc('/pages/glowing-moon-fadeout.gif')
                 setTimeout(() => {
-                    navigate('/new-star')
-                }, 2000)
-            })
-        }
-    }, []);
+                    setShow(false)
+                    setTimeout(() => {
+                        navigate('/new-star')
+                    }, 3000)
+                }, 3000)
+            }, 5000)
+        }, 2500)
+    }, [])
 
     return (
         <BlankPage bg='plain-space.gif'>
             <Helmet>
+                <link rel="prefetch" href="/pages/glowing-moon.gif" as="image" type="image/gif" crossOrigin="anonymous" />
+                <link rel="prefetch" href="/pages/glowing-moon-fadeout.gif" as="image" type="image/gif" crossOrigin="anonymous" />
                 <link rel="prefetch" href="/images/abstract.jpg" as="image" type="image/jpeg" crossOrigin="anonymous" />
             </Helmet>
-            <div className='[&>p]:text-lg/relaxed absolute z-10 top-[16%] ani-delay-500ms fade-in'>
-                <p className='fade-out ani-delay-4s'>
+            <div className='[&>p]:text-lg/relaxed absolute z-20 top-[16%] fade-in'>
+                <p className='fade-out ani-delay-3s'>
                     ทันใดนั้น ดวงจันทร์ก็เกิดแสงสว่างขึ้น
                 </p>
             </div>
-            <div className='[&>p]:text-lg/relaxed absolute z-10 top-[16%] ani-delay-5s fade-in'>
-                <p className='fade-out ani-delay-6s'>
+            <div className='[&>p]:text-lg/relaxed absolute z-20 top-[16%] ani-delay-4s fade-in'>
+                <p className='fade-out ani-delay-5s'>
                     แสงสว่างจ้าขึ้นเรื่อย ๆ
                 </p>
             </div>
-            <div className='[&>p]:text-xl/relaxed z-10 ani-delay-8s fade-in'>
-                <p className='fade-out ani-delay-10s text-black'>
+            <div className='[&>p]:text-xl/relaxed z-20 ani-delay-6s fade-in'>
+                <p className='fade-out ani-delay-8s text-black'>
                     ....
                 </p>
             </div>
-            <div className='[&>p]:text-lg/relaxed absolute z-10 top-[16%] ani-delay-11s fade-in'>
-                <p className='fade-out ani-delay-14s'>
+            <div className='[&>p]:text-lg/relaxed absolute z-20 top-[16%] ani-delay-9s fade-in'>
+                <p className='fade-out ani-delay-13s'>
                     คุณเริ่มเห็นดาวดวงใหม่ปรากฎขึ้น
                 </p>
             </div>
             <img className='pointer-events-none scale-[0.85] z-1 absolute fade-in ani-delay-13s' src='/images/moon.png' alt='moon' />
-            <div className='absolute w-full h-full fade-out ani-delay-14s'>
-                <video ref={videoPlayer} className="fade-in ani-delay-500ms z-[1] object-cover absolute w-full h-full" muted playsInline>
-                    <source src={"/pages/glowing-moon.mp4"} type="video/mp4" />
-                </video>
-                <video ref={videoPlayer2} className="fade-in ani-delay-10s z-[2] object-cover absolute w-full h-full" muted playsInline>
-                    <source src={"/pages/glowing-moon-fadeout.mp4"} type="video/mp4" />
-                </video>
-            </div>
-            {/* <img className='overlay fade-in' src='/pages/glowing-moon.gif' alt='moon-glowing' /> */}
-            <img className='ani-delay-1s fade-out overlay pointer-events-none scale-[0.85]' src='/images/moon.png' alt='moon' />
+            <>{show && <img className='overlay fade-in' src={src} alt='moon-glowing' />}</>
+            <img className='ani-delay-4s fade-out overlay pointer-events-none scale-[0.85]' src='/images/moon.png' alt='moon' />
+            <img className='ani-delay-9s fade-in overlay pointer-events-none scale-[0.85]' src='/images/moon.png' alt='moon' />
         </BlankPage>
     )
 }
